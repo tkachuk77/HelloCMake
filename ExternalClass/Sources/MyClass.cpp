d4213b58baad73a31e6f41f1d997ea3ecf283b7e
Alex <tkachuk77@gmail.com>
2021-10-25 20:46:07 +0300

    init

---
+++ ExternalClass/Sources/MyClass.cpp
@@ -0,0 +1,6 @@
+
+#include "../../ExternalClass/Headers/MyClass.hpp"
+
+void MyClass::Show(){
+    std::cout<< "Hello entry Class!!!" << std::endl;
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "ExternalClass/Headers/MyClass.hpp"
+int main(int, char**) {
+
+    MyClass t;
+    t.Show();
+
+    std::cout << "Hello, world!\n";
+}
